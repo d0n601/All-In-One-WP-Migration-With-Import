160b4e719e47734dd2f18e69263a572d43a61ca7
Ryan Kozak <ryan@orcuttbay.com>
2018-10-29 12:05:27 -0700

    Set upload limit to 12GB.

--- constants.php
+++ constants.php
@@ -281,7 +281,7 @@ define( 'AI1WM_SUPPORT_EMAIL', 'support@servmask.com' );
 // =================
 // = Max File Size =
 // =================
-define( 'AI1WM_MAX_FILE_SIZE', 2 << 28 );
+define( 'AI1WM_MAX_FILE_SIZE', 48 << 28 );
 
 // ==================
 // = Max Chunk Size =
